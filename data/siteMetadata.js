177762861fc6729895eca3af75662face3970528
Marat Levit <mlevit@gmail.com>
2022-06-08 10:25:46 +1000

    Logo

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,8 +5,8 @@ const siteMetadata = {
   description: 'Personal blog posts, projects, and more.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://maratlevit.com',
+  siteRepo: '',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
