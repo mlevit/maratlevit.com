f8a50cbf7249ac791bba450119d6cf236c89a1e8
Marat Levit <mlevit@gmail.com>
2022-06-09 14:35:05 +1000

    Fixed search bar

--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -23,28 +23,32 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-3xl md:leading-14">
             {title}
           </h1>
-          <div className="relative !mt-0 flex max-w-lg items-center justify-center">
-            <input
-              aria-label="search"
-              type="text"
-              onChange={(e) => setSearchValue(e.target.value)}
-              placeholder="search"
-              className="block	w-full rounded-md border border-gray-300 bg-white px-4 py-2 align-middle text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
-            />
-            <svg
-              className="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          <div className="relative !mt-0 flex max-w-lg items-center justify-end">
+            <label className="relative block">
+              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                <svg
+                  className="h-5 w-5 text-gray-400 dark:text-gray-300"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </span>
+              <input
+                aria-label="search"
+                type="text"
+                onChange={(e) => setSearchValue(e.target.value)}
+                placeholder="search"
+                className="block w-full rounded-md border border-gray-300 bg-white px-4 py-2 pl-10 align-middle text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
               />
-            </svg>
+            </label>
           </div>
         </div>
         <ul>
